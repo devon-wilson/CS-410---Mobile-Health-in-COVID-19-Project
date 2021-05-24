61968c1bdd0d8b6628231604b5512c42657d6048
Devon <devon23@pdx.edu>
2021-05-24 15:11:52 -0700

    my initial commit

---
+++ src/Components/Calendar.js
@@ -0,0 +1,93 @@
+import React, { Component } from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import axios from "axios";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import "./calendar.css";
+
+moment.locale("en-GB");
+
+const localizer = momentLocalizer(moment);
+
+class ChemCalendar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cal_events: [
+        //State is updated via componentDidMount
+      ],
+    };
+  }
+
+  convertDate = (date) => {
+    return moment.utc(date).toDate();
+  };
+
+  async componentDidMount() {
+    const obj = [
+      {
+        start: new Date("2021-05-02"),
+        end: new Date("2021-05-02"),
+        name: "test product",
+        allDay: true,
+        title: "chemapp",
+      },
+
+      {
+        start: new Date("2021-05-02"),
+        end: new Date("2021-05-02"),
+        name: " product",
+        allDay: true,
+        title: "diff",
+      },
+    ];
+    this.setState({
+      cal_events: [...obj],
+    });
+  }
+
+  eventStyleGetter(event, start, end, isSelected) {
+    console.log(event);
+    var backgroundColor = "#" + event.hexColor;
+    var style = {
+      backgroundColor: backgroundColor,
+      borderRadius: "0px",
+      opacity: 0.8,
+      color: "black",
+      border: "0px",
+      display: "block",
+    };
+    return {
+      style: style,
+    };
+  }
+
+  render() {
+    const { cal_events } = this.state;
+
+    return (
+      <div className="Calendar">
+        <header className="calendar-header">
+          <h1 className=".calendar-title">React Calendar</h1>
+        </header>
+        <div style={{ height: 700 }}>
+          <Calendar
+            localizer={localizer}
+            events={cal_events}
+            //step={960}
+            defaultView="month"
+            views={["month", "week", "day"]}
+            defaultDate={new Date()}
+            onSelectSlot={this.slotSelected}
+            onSelectEvent={this.eventSelected}
+            eventPropGetter={this.eventStyleGetter}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ChemCalendar;
---
+++ src/Components/Navigation.js
@@ -0,0 +1,55 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { Route, Switch, Link, BrowserRouter as Router } from "react-router-dom";
+import Home from "./Home";
+import CalendarView from "./Calendar";
+// import AddForm from "./AddForm";
+// import SearchForm from "./SearchForm";
+// import Templates from "./Templates/Templates";
+// import CreateTemplate from "./Templates/Create/CreateTemplate";
+// import EditTemplate from "./Templates/Edit/EditTemplate";
+
+function Navigation(props) {
+    return (
+        <Router>
+            <div>
+                <Navbar bg="primary" variant="dark">
+                    <Nav className="mr-auto">
+                        <Nav.Link as={Link} to="/">Home</Nav.Link>
+                        <br></br>
+                        <Nav.Link as={Link} to="/Calendar">Calendar</Nav.Link>
+                        {/* <Nav.Link as={Link} to="/Add_Form">Add Form</Nav.Link>
+                        <Nav.Link as={Link} to="/Search_Form">Search Forms</Nav.Link>
+                        <Nav.Link as={Link} to="/Templates">Templates</Nav.Link> */}
+                    </Nav>
+                </Navbar>
+                <Switch>
+                    <Route path="/Calendar">
+                        <CalendarView />
+                    </Route>
+                    {/* <Route path="/Add_Form">
+                        <AddForm />
+                    </Route>
+                    <Route path="/Search_Form">
+                        <SearchForm/>
+                    </Route>
+                    <Route path="/Templates">
+                        <Templates />
+                    </Route>
+                    <Route path="/CreateTemplate">
+                        <CreateTemplate />
+                    </Route>
+                    <Route path="/EditTemplate">
+                        <EditTemplate />
+                    </Route> */}
+                    <Route path="/">
+                        <Home />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
+}
+
+export default Navigation;
