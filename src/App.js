aa01a79c180cadcdbb82c99ab3a211cad5b1e26b
Isaiah Shikina <ishikina@pdx.edu>
2021-05-23 23:57:34 -0700

    Added navigation bar

--- src/App.js
+++ src/App.js
@@ -3,14 +3,34 @@ import "./App.css";
 import Calendar from "./components/Calendar"
 import Chart from "./components/Chart"
 import Analytics from "./components/Analytics"
+import { Tab, Tabs, AppBar } from "@material-ui/core"
+import { Route, Switch, Link, BrowserRouter as Router } from "react-router-dom"
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 
 function App() {
+  
   return (
+    <Router>
       <div>
-        <Calendar />
-        <Chart />
-        <Analytics />
-      </div>
+        <Navbar class="primary" variant="dark">
+          <Nav.Link as={Link} to="/Calendar">Calendar</Nav.Link>
+          <Nav.Link as={Link} to="/Chart">Progress</Nav.Link>
+          <Nav.Link as={Link} to="/Analytics">Analytics</Nav.Link>
+        </Navbar>
+        <Switch>
+          <Route path="/Calendar">
+            <Calendar />
+          </Route>
+          <Route path="/Chart">
+            <Chart />
+          </Route>
+          <Route path="/Analytics">
+            <Analytics />
+          </Route>
+        </Switch> 
+      </div>   
+    </Router>
   );
 }
 
--- src/components/Analytics.js
+++ src/components/Analytics.js
@@ -1,6 +1,40 @@
-import React from 'react';
+import React from 'react'
+import { Line , Pie } from 'react-chartjs-2'
+import axios from 'axios'
+import Backend from './Backend'
 
-const Analytics = () => {
-    return <div>Analytics</div> 
+class Analytics extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            data: []
+        }
+    }
+  
+    async componentDidMount(){
+        const today = new Date() 
+        const month = today.getMonth() + 1
+        let backend = new Backend()
+        let response = await backend.getMonth(month)
+        this.setState({data: response})
+        return response
+    }
+  
+    log(){
+        console.log(this.state)
+    }    
+    
+   
+    returnOptions(){
+    }
+
+    render(){
+        return (
+            <div>
+                Analytics
+            </div>
+          )
+    }
 }
+
 export default Analytics;
